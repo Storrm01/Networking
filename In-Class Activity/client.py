1088248e03b9b9766a11a2038f8b8f7da2118589
Matthew <matthewt2356@gmail.com>
2025-09-17 12:13:04 -0500

    first commit

---
+++ In-Class Activity/client.py
@@ -0,0 +1,25 @@
+from socket import *
+
+# Pre-requisites
+    # we need some info
+serverIP = '127.0.0.1'
+serverPort = 13000
+serverAddr = (serverIP, serverPort)
+
+# my own information (not required!)
+myIP = '127.0.0.1'
+myPort = 10101
+
+# 1. Take in a message to send
+msg = input('Please enter a message to send: ')
+
+# 2. Encode this (prepare it for transfer)
+encoded = msg.encode()
+
+# 3. Send it to someone
+    # Create a Socket to enable sending/receiving 
+mySocket = socket(AF_INET, SOCK_DGRAM)
+
+mySocket.sendto(encoded, serverAddr)
+
+print(mySocket) 
---
+++ In-Class Activity/server.py
@@ -0,0 +1,22 @@
+from socket import *
+
+myIP = '127.0.0.1'
+myPort = 13000
+myAddr = (myIP, myPort)
+
+# Create a socket to listen
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Bind this socket to a port
+serverSocket.bind(myAddr)
+
+# keep receiving
+while(True) :
+    # receive some message
+    received, clientAddr = serverSocket.recvfrom(2048)
+
+    # Decode and Display the message received
+    decoded = received.decode()
+    print(decoded)
+    if decoded == 'exit':
+        break
